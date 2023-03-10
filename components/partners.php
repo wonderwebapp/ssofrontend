98d626a0a1d31ea1d33dd62ebfc1f336638b7d3e
AbedelrahmanD <81683407+abedelrahmanOpenTech@users.noreply.github.com>
2023-03-10 21:42:03 +0200

    3d slider

--- components/partners.php
+++ components/partners.php
@@ -12,14 +12,12 @@
     </div>
     <section class="partners-logos slider" >
         <div class="slide"><img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"></div>
-        <div class="slide"><img src="http://www.webcoderskull.com/img/logo.png"></div>
         <div class="slide"><img src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg"></div>
         <div class="slide"><img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"></div>
         <div class="slide"><img src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg"></div>
         <div class="slide"><img src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg"></div>
         <div class="slide"><img src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg"></div>
         <div class="slide"><img src="https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg"></div>
-        <div class="slide"><img src="https://image.freepik.com/free-vector/retro-label-on-rustic-background_82147503374.jpg"></div>
     </section>
 
 
--- components/slider.php
+++ components/slider.php
@@ -1,33 +1,34 @@
-<link rel="stylesheet" href="css/slider.css?v=<?= $v?>">
+<link rel="stylesheet" href="css/slider.css?v=<?= $v ?>">
+<script src="js/slider.js?v=<?= $v ?>"></script>
 
+<section id="home" class="">
+  <div class="wrapper">
+    <div class="main">
+      <?php for ($i = 1; $i <= 3; $i++) : ?>
+        <section class="">
+          <div class="page_container d-flex justify-content-center align-items-center w-100 h-100"
+          style="background: url(https://cdn.pixabay.com/photo/2016/09/29/13/08/planet-1702788_960_720.jpg) center center;
+  background-size: cover;"
+          >
+            <h1 class="text-center w-100">
+              <a href="http://www.bucketlistly.com/achievements/visit-sydney-australia-95861" target="_blank">Travel to
+                Sydney, Australia<br>
+                <small>Completed on 14 Feb 2014</small></a>
+            </h1>
+          </div>
+        </section>
 
-<section id="home" class="dvd_fixed_slider w-100 overflow-hidden">
-  <div class="slides_bg">
-    <div class="w-100 position-relative" style="background-image: url('https://cdn.pixabay.com/photo/2016/09/29/13/08/planet-1702788_960_720.jpg')">
-      <div class="w-100 position-absolute top-0 bottom-0 start-0 right-0 bg-dark-transparent d-flex flex-column justify-content-center align-items-center">
-        <h1 class="text-white">PAGE1</h1>
-        <p class="text-white text-center col-11 col-md-6 mx-auto">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi et laborum, quis delectus obcaecati consequuntur incidunt quidem. At, dolorum et.
-        </p>
-      </div>
-    </div>
-    <div class="w-100 position-relative" style="background-image: url('https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067_960_720.png')">
-      <div class="w-100 position-absolute top-0 bottom-0 start-0 right-0 bg-dark-transparent d-flex flex-column justify-content-center align-items-center">
-        <h1 class="text-white">PAGE2</h1>
-        <p class="text-white text-center col-11 col-md-6 mx-auto">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi et laborum, quis delectus obcaecati consequuntur incidunt quidem. At, dolorum et.
-        </p>
-      </div>
-    </div>
-    <div class="w-100 position-relative" style="background-image: url('https://cdn.pixabay.com/photo/2012/04/14/16/37/sky-34536_960_720.png')">
-      <div class="w-100 position-absolute top-0 bottom-0 start-0 right-0 bg-dark-transparent d-flex flex-column justify-content-center align-items-center">
-        <h1 class="text-white">PAGE3</h1>
-        <p class="text-white text-center col-11 col-md-6 mx-auto">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi et laborum, quis delectus obcaecati consequuntur incidunt quidem. At, dolorum et.
-        </p>
-      </div>
-    </div>
+      <?php endfor; ?>
 
+    </div>
   </div>
 
 </section>
+
+<script>
+  $(document).ready(function() {
+    $(".main").tiltedpage_scroll({
+      angle: 45
+    });
+  });
+</script>
